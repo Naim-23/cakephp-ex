322aed515d462e1b9563ac7939a92c0c8b2a9dba
Naim-23 <78157234+Naim-23@users.noreply.github.com>
2021-04-08 20:14:06 +0100

    Add files via upload

--- webroot/MP/Logout.php
+++ webroot/MP/Logout.php
@@ -3,7 +3,7 @@
 session_unset();
 session_destroy();
 
-header("location: LoginForm.php");
+header("location: http://cakephp-mysql-persistent-qmweblab.apps.okd.eecs.qmul.ac.uk/MP/PortfolioFrontPage.html");
 
 exit();
 
