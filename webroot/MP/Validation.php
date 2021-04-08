dda6e5e92ee20e0c97c3b883ae08e7e6119a9ec5
Naim-23 <78157234+Naim-23@users.noreply.github.com>
2021-04-08 14:41:41 +0100

    Add files via upload

--- webroot/MP/Validation.php
+++ webroot/MP/Validation.php
@@ -9,8 +9,9 @@ if (isset($_POST['email'])) {
   $email=$_POST['email'];
   $password=$_POST['password'];
 
-  $value = mysqli_query($conn, "SELECT * FROM LOGIN WHERE email = '$email' AND password = '$password'");
-  if(mysqli_num_rows($value)==1) {
+  $value = mysqli_query($conn, "SELECT * FROM LOGIN1 WHERE email = '$email' AND password = '$password' ");
+  if(mysqli_num_rows($value)==1)
+  {
      header("location: Blog.php");
   }
 
